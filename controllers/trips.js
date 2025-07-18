939c5cd728c249f032fb9b5ea2710a7375950e1f
Ryan B <bombingrun3@gmail.com>
2025-07-18 13:51:47 -0500

    Setup initial communication to trips controller

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,3 +1,9 @@
-const getTrips = (req, res) => {};
-
-module.exports = getTrips;
+const getTrips = (req, res) => {
+  try {
+    res.send("Trips controller works");
+  } catch (error) {
+    console.error("There was an error getting your trips:", error);
+    res.status(500).send("There was an error getting your trips");
+  }
+};
+module.exports = { getTrips };
