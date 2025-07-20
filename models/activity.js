dcbf76c1907df99832ae202d9097b1dfb8805052
Ryan B <bombingrun3@gmail.com>
2025-07-20 01:19:44 -0500

    Added enums and an image parameter for activy schema

--- models/activity.js
+++ models/activity.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 
 const activitySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  type: { type: String, required: true },
-  difficulty: { type: String, required: true },
+  type: { type: String, enum: ["hiking", "rafting", "biking"], required: true },
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    required: true,
+  },
   location: {
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
   },
   durationHours: { type: Number, required: true },
+  image: { type: String },
 });
 
 module.exports = mongoose.model("Activity", activitySchema);
