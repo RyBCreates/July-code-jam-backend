610ca2f56e66d3ad4c308c47e2ca0ecf9f59abf8
Ryan B <bombingrun3@gmail.com>
2025-07-20 03:02:10 -0500

    Added CRUD for trips and setup routes

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,19 +1,80 @@
-// get all (user's) trips
-const getTrips = (req, res) => {
+const Trip = require("../models/trip");
+
+// Get all trips for a user
+const getAllTrips = async (req, res) => {
   try {
-    res.send("Trips controller works");
-  } catch (error) {
-    console.error("There was an error getting your trips:", error);
-    res.status(500).send("There was an error getting your trips");
+    const trips = await Trip.find({ userId: req.user._id }).populate(
+      "activities.activityId"
+    );
+    res.status(200).json(trips);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to get trips", error: err.message });
   }
 };
 
-// Create a trip
+// Get a specific trip
+const getTripById = async (req, res) => {
+  try {
+    const trip = await Trip.findOne({
+      _id: req.params.id,
+      userId: req.user._id,
+    }).populate("activities.activityId");
 
-//get one trip
+    if (!trip) {
+      return res.status(404).json({ message: "Trip not found" });
+    }
+
+    res.status(200).json(trip);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get trip", error: err.message });
+  }
+};
+
+// Create a new trip
+const createTrip = async (req, res) => {
+  try {
+    const { name, activities } = req.body;
+
+    const newTrip = new Trip({
+      name: name || "Untitled Trip",
+      userId: req.user._id,
+      activities,
+    });
+
+    const savedTrip = await newTrip.save();
+    res.status(201).json(savedTrip);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Failed to create trip", error: err.message });
+  }
+};
 
 // Delete a trip
+const deleteTrip = async (req, res) => {
+  try {
+    const deleted = await Trip.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
 
-// Update a trip (MAYBE)
+    if (!deleted) {
+      return res.status(404).json({ message: "Trip not found" });
+    }
 
-module.exports = { getTrips };
+    res.status(200).json({ message: "Trip deleted successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to delete trip", error: err.message });
+  }
+};
+
+module.exports = {
+  getAllTrips,
+  getTripById,
+  createTrip,
+  deleteTrip,
+};
--- routes/trips.js
+++ routes/trips.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { getTrips } = require("../controllers/trips");
+const {
+  getAllTrips,
+  getTripById,
+  createTrip,
+  deleteTrip,
+} = require("../controllers/trips");
 
 // Get all recipes
-router.get("/", getTrips);
+router.get("/", getAllTrips);
+// Get one Trip by Id
+router.get("/:id", getTripById);
+// Create A Trip
+router.post("/", createTrip);
+// Delete A Trip
+router.delete("/:id", deleteTrip);
 
 module.exports = router;
