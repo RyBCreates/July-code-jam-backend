479ec7a44b34ab0ae3fb878b474675607e28c8c0
Ryan B <bombingrun3@gmail.com>
2025-07-20 01:16:19 -0500

    added User Schema

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+    },
+  },
+  password: { type: String, required: true, select: false },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (matched) {
+          return user;
+        }
+
+        return Promise.reject(new Error("Incorrect email or password"));
+      });
+    });
+};
+
+module.exports = mongoose.model("User", userSchema);
