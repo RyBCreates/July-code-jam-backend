b40bbbb74519ad846d173cd402faf5d22e1079ea
Ryan B <bombingrun3@gmail.com>
2025-07-22 10:39:16 -0500

    Fixed naming error and deleted unneeded files

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 
-const User = require("../models/User");
+const User = require("../models/user");
 // const {
 //   BadRequestError,
 //   NotFoundError,
